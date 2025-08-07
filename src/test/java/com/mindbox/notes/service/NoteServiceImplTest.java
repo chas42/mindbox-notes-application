6117a977d6e8ec8aaefc8de7789e836448f0911e
César Santos <cesar.santos@pitang.com>
2025-08-06 23:20:04 -0300

    Update Maven configuration and add H2 dependency; enhance test profiles

--- src/test/java/com/mindbox/notes/service/NoteServiceImplTest.java
+++ src/test/java/com/mindbox/notes/service/NoteServiceImplTest.java
@@ -1,9 +1,13 @@
 package com.mindbox.notes.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class NoteServiceImplTest {
 
     @Test
