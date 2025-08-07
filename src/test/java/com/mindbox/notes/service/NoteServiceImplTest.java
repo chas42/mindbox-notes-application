83f54577fc4eb7751a37cf8e153de60d5e127c2e
César Santos <cesar.santos@pitang.com>
2025-08-06 23:19:16 -0300

    Add application-test.yml and NoteServiceImplTest class for testing setup

---
+++ src/test/java/com/mindbox/notes/service/NoteServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.mindbox.notes.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteServiceImplTest {
+
+    @Test
+    void create() {
+    }
+}
